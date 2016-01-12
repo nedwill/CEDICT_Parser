5b7eec23071b486ddcb88bfc5dfbc75edaf7c874
Ned Williamson <nedwilliamson@gmail.com>
2016-01-12 13:22:13 -0500

    fixing parser to suit my needs

--- parser.py
+++ parser.py
@@ -1,28 +1,4 @@
 from pinyin import *
-
-def write_file(file_name, dic):
-	new_file = open(file_name, 'w')
-	
-	new_file.write("var CH_DIC = function() {")
-	new_file.write("\n\n")
-	new_file.write("\tthis._table = {")
-	new_file.write("\n\n")
-	
-	for item in dic:
-		try:
-			new_file.write("\t\t")
-			new_file.write("\"" + item["hanzi"] + "\":")
-			new_file.write(" \"" + item["pinyin"] + "|" + item["def"] + "\",")
-			new_file.write('\n')
-		except:
-			print("Something went wrong")
-	
-	new_file.write("\n")
-	new_file.write("\t}")
-	new_file.write("\n\n")
-	new_file.write("}")
-	
-	new_file.close()
 	
 def read_file(file_name):
 	
@@ -35,25 +11,36 @@ def read_file(file_name):
 	f = open(file_name, "r")
 	lines = f.readlines()
 	
-	for line in lines:
-		l = line
+	for line in lines[500:505]:
+		#print(line)
+		l = line.strip()
 		
 		#These are info lines at the beginning of the file
 		#NOTE: Might be useful to store version #, date, etc for dictionary reference
 		if l.startswith(("#", "#!")):
 			continue
-		else:
-			#partition out definition text, replace slshes with semicolons, normalize quotations, get rid of any \n
-			defi = l.partition('/')[2].replace('/','; ').replace("\"", "'").strip()
-			#Get trad and simpl hanzis then split and take only the simplified
-			han = l.partition('[')[0].split(' ', 1)[1].strip(" ")
-			#Take the content in between the two brackets
-			pin = l.partition('[')[2].partition(']')[0]
-			
-			pin = convert(pin);			
-			
-			items.append({"hanzi":han, "pinyin":pin, "def":defi})
+
+		#partition out definition text, replace slshes with semicolons, normalize quotations, get rid of any \n
+		assert l.partition('/')[2].split('/')[-1] == ""
+		defi_list = l.partition('/')[2].split('/')[:-1]
+		#Get trad and simpl hanzis then split and take only the simplified
+		trad, simp = [e.strip(" ") for e in l.partition('[')[0].split(' ', 1)]
+		#Take the content in between the two brackets
+		pin = l.partition('[')[2].partition(']')[0]
+		#TODO: make accented pin
+		print(l.strip())
+		print(defi_list)
+		print(trad)
+		print(simp)
+		print(pin)
+		print("")
+		
+		pin = convert(pin);			
+		
+		#items.append({"hanzi":han, "pinyin":pin, "def":defi})
 	
-	write_file("CH_DIC.js", items)
+	#for e in items:
+	#	print(e)
+	#write_file("CH_DIC.js", items)
 		
 read_file("cedict_1_0_ts_utf-8_mdbg.txt")
