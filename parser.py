727bc50f0d3e7666a5a3a9a069c4373afd9d86b9
Ned Williamson <nedwilliamson@gmail.com>
2016-02-03 13:33:29 -0500

    making flashcards for the whole semester

--- parser.py
+++ parser.py
@@ -1,31 +1,107 @@
 from pinyin import *
-	
-def read_file(file_name):
-	
-	# EXAMPLE INPUT LINE:   㐖 㐖 [Ye4] /see
-	# TRADITIONAL_HANZI SIMPLIFIED_HANZI [PINYIN] /TRANSLATION
-	
-	# Put each dictionary item into the array
-	items = []
+import argparse
 
-	f = open(file_name, "r")
-	lines = f.readlines()
-	
-	for line in lines[500:505]:
-		l = line.strip()
-		
-		#These are info lines at the beginning of the file
-		#NOTE: Might be useful to store version #, date, etc for dictionary reference
-		if l.startswith(("#", "#!")):
+DICTFILE = "cedict_1_0_ts_utf-8_mdbg.txt"
+
+def print_item(item):
+	print("Word number: {}".format(item["i"]))
+	print("Traditional: {}".format(item["trad"]))
+	print("Simplified: {}".format(item["simp"]))
+	print("Pinyin: {}".format(item["pin"]))
+	print("Definitions: {}".format(item["defns"]))
+	print("")
+
+def read_file(process_item):
+	lines = open(DICTFILE, "r").readlines()
+	for i, _line in enumerate(lines):
+		line = _line.strip()
+
+		if line.startswith(("#", "#!")): #skip comments
 			continue
 
-		assert l.partition('/')[2].split('/')[-1] == ""
-		defns = "; ".join(l.partition('/')[2].split('/')[:-1])
-		#Get trad and simpl hanzis then split and take only the simplified
-		trad, simp = [e.strip(" ") for e in l.partition('[')[0].split(' ', 1)]
-		#Take the content in between the two brackets
-		pin = decode_pinyin(l.partition('[')[2].partition(']')[0])
-		items.append({"trad":trad, "simp":simp, "defns":defns, "pin": pin})
-	print(items)
-		
-read_file("cedict_1_0_ts_utf-8_mdbg.txt")
+		#assumption about formatting of dict: trailing '/' on each line
+		assert line.partition('/')[2].split('/')[-1] == ""
+
+		defns = "; ".join(line.partition('/')[2].split('/')[:-1])
+		trad, simp = [e.strip(" ") for e in line.partition('[')[0].split(' ', 1)]
+		pin = decode_pinyin(line.partition('[')[2].partition(']')[0])
+
+		item = {"i": i, "trad":trad, "simp":simp, "defns":defns, "pin": pin}
+		process_item(item)
+
+def search_file(search_term):
+	def search_process_item(item):
+		#substring of definition or matches the character/pinyin exactly
+		if search_term == item["simp"]:
+#		if search_term in item["defns"] or search_term in [item["trad"], item["simp"], item["pin"]]:
+			print(search_term, item["pin"])
+			#print_item(item)
+	read_file(search_process_item)
+
+def simp_to_pinyin_dict():
+	import json
+	return json.loads(open('temp.json', 'r').read())
+
+	d = {}
+	def process(item):
+		d[item["simp"]] = item
+	read_file(process)
+	import json
+	open('temp.json', 'w').write(json.dumps(d))
+	return d
+
+def format_anki(numbers):
+	pass
+
+def main():
+	parser = argparse.ArgumentParser(description="Ned's Chinese Anki Card Assistant")
+	parser.add_argument('--search', dest='search_term', type=str, help='search term')
+	parser.add_argument('--words', dest='word_numbers', metavar='word_number', nargs='+', type=int, help='word numbers to make cards from')
+	#parser.add_argument('--dest', dest='dest', default=None, help='destination for anki output (default stdout)')
+	args = parser.parse_args()
+
+	if args.search_term is None and args.word_numbers is None:
+		parser.print_help()
+		exit()
+
+	if args.search_term is not None and args.word_numbers is not None:
+		print("Error: cannot both search and print word numbers.")
+		exit()
+
+	if args.search_term is not None:
+		search_file(args.search_term)
+	else:
+		assert args.word_numbers is not None
+		format_anki(args.word_numbers)
+
+webcore = open('webcore', 'r').read()
+d = simp_to_pinyin_dict()
+for line in webcore.splitlines():
+	if line == "":
+		print(line)
+		continue
+	simp, defn = line.split("\t")
+	if simp not in d:
+		#print("[*] Warning: {} not found, constructing pinyin from constituent characters.".format(simp))
+		pin = "".join(d[c]['pin'] for c in simp)
+		trad = "".join(d[c]['trad'] for c in simp)
+	else:
+		pin = d[simp]['pin']
+		trad = d[simp]['trad']
+
+	print("\t".join((simp, trad, pin, defn)))
+
+	#print(line)
+
+exit()
+
+chars = ['们', '在', '跟', '问', '不', '四', '文', '表', '作', '有', '说', '吗', '习', '考', '一', '家', '客', '二', '生', '声', '题', '试', '请', '谢', '怎', '你', '中', '课', '么', '三', '同', '再', '气', '很', '懂', '练', '第', '好', '念', '页', '下', '师', '上', '英', '打', '演', '学', '词', '看', '开', '几', '写', '书', '大', '遍', '了', '老', '对', '没', '业', '轻', '见', '我']
+for simp in chars:
+	if simp not in d:
+		print("[-] {} not found, exiting.".format(simp))
+		exit()
+	ch_info = d[simp]
+	#print(ch_info)
+	print("{}\t{}\t{}\t{}".format(ch_info['trad'], ch_info['simp'], ch_info['pin'], ch_info['defns']))
+
+#simp_to_pinyin_dict()
